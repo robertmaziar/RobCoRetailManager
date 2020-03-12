f18edf7761c1b1a9c33de6d362f85f13c2f43187
Robert Maziar <robert.maziar@gmail.com>
2020-03-11 21:24:05 -0400

    Wire Up Login Form to API
    Connects the login form button to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed.

---
+++ RRMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using RRMDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace RRMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
