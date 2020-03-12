853fc515023d5b3faf5ba69d5020d4a8761bc410
Robert Maziar <robert.maziar@gmail.com>
2020-03-12 14:36:24 -0400

    Initial Setup to Retrieve User Data
    Initial setup done to retrieve user data using the API. Added the API Library project to the solution.

---
+++ RRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using RRMDataManager.Library.DataAccess;
+using RRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            UserData data = new UserData();
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
