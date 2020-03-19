1232ef3184cf1a096bbc915ac571cb494fb825f7
Robert Maziar <robert.maziar@gmail.com>
2020-03-19 18:50:08 -0400

    Get Add To Cart Working
    Adds the funtionality to add items to the cart,
    get the subtotal, and update quantities.

--- RRMDesktopUI/ViewModels/SalesViewModel.cs
+++ RRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,16 +1,13 @@
 ﻿using Caliburn.Micro;
 using RRMDesktopUI.Library.Api;
 using RRMDesktopUI.Library.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RRMDesktopUI.ViewModels
 {
-    public class SalesViewModel : Screen
+	public class SalesViewModel : Screen
     {
 		private IProductEndpoint _productEndpoint;
 
@@ -43,9 +40,22 @@ namespace RRMDesktopUI.ViewModels
 			}
 		}
 
-		private BindingList<ProductModel> _cart;
+		private ProductModel _selectedProduct;
 
-		public BindingList<ProductModel> Cart
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set 
+			{	
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+		public BindingList<CartItemModel> Cart
 		{
 			get { return _cart; }
 			set
@@ -55,7 +65,7 @@ namespace RRMDesktopUI.ViewModels
 			}
 		}
 
-		private int _itemQuantity;
+		private int _itemQuantity = 1;
 
 		public int ItemQuantity
 		{
@@ -64,15 +74,22 @@ namespace RRMDesktopUI.ViewModels
 			{ 
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
 		public string SubTotal
 		{
 			get 
-			{ 
-				// TODO - Replace w/ calulation
-				return "0.00"; 
+			{
+				decimal subTotal = 0;
+
+				foreach (var item in Cart)
+				{
+					subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+				}
+
+				return subTotal.ToString("C"); 
 			}
 		}
 
@@ -81,7 +98,7 @@ namespace RRMDesktopUI.ViewModels
 			get
 			{
 				// TODO - Replace w/ calulation
-				return "0.00";
+				return "$0.00";
 			}
 		}
 
@@ -90,7 +107,7 @@ namespace RRMDesktopUI.ViewModels
 			get
 			{
 				// TODO - Replace w/ calulation
-				return "0.00";
+				return "$0.00";
 			}
 		}
 
@@ -100,14 +117,40 @@ namespace RRMDesktopUI.ViewModels
 			{
 				bool output = false;
 
-				// Make sure something is selected
-				// Make sure there is an item quantity
+				if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+				{
+					output = true;
+				}
 
 				return output;
 			}
 		}
 
-		public void AddToCart() { }
+		public void AddToCart() 
+		{
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+			if (existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+				// HACK - There should be a better way of refreshing the cart display
+				Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel item = new CartItemModel
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+				Cart.Add(item);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
+		}
 
 		public bool CanRemoveToCart
 		{
@@ -121,7 +164,10 @@ namespace RRMDesktopUI.ViewModels
 			}
 		}
 
-		public void RemoveFromCart() { }
+		public void RemoveFromCart() 
+		{
+			NotifyOfPropertyChange(() => SubTotal);
+		}
 
 		public bool CanCheckOut
 		{
